84bb3bbb978274571b3ef29e768e4293eb9707a8
mohamed rhzaoui <mohamed1rhzaoui@gmail.com>
2021-03-06 16:43:42 +0100

    initial

---
+++ style.js
@@ -0,0 +1,30 @@
+
+ //let btnqwe1 = document.querySelector('#qwe-1');
+ //let btnqwe2 = document.querySelector('#qwe-2');
+// let btnqwe3 = document.querySelector('#qwe-3')
+ //let body = document.querySelector('#asd-body');
+
+ //btnqwe1.addEventListener('click',() => body.style.background='linear-gradient(to right,yellow,#fff)' )
+// btnqwe2.addEventListener('click',() => body.style.background='linear-gradient(to right,red,#fff)' )
+ //btnqwe3.addEventListener('click',() => body.style.background='linear-gradient(to right,#000,rgb(170, 161, 161)' )
+
+
+ //var color = ["linear-gradient(to right,#B91372,#fff)","linear-gradient(to right,red,#fff)","linear-gradient(to right,#000,rgb(170, 161, 161)"]
+//var i =0;
+//document.querySelector("span").addEventListener("click",
+//function(){
+ // i=i<color.length? ++i : 0;
+ // document.querySelector("body").style.background= color[i]
+//})
+
+//var color1=[" linear-gradient(to right,rgb(186, 186, 236),rgb(66, 66, 238))"]
+
+//document.querySelector('#pointer').addEventListener("click",
+//function(){
+  
+  //document.querySelector("body").style.background = color1
+//}
+//)
+//function myfuncyion(){
+ // document.querySelector("body").style.background =" linear-gradient(to right,rgb(186, 186, 236),rgb(66, 66, 238))"
+//}
